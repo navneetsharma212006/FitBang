94d21cb43022ae552ae9951822c844eb57430aa9
navneetsharma212006 <navneetsh21@gmail.com>
2026-06-21 09:14:15 +0530

    Configure dynamic backend URL and CORS origin for deployment compatibility

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
